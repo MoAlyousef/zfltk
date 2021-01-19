eb6bf7491dbe7c781f8c18544bc2c4110ca68219
Mohammed Alyousef <may642_2000@hotmail.com>
2021-01-19 19:52:13 +0300

    styling

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ Run:
 $ zig build
 $ ./zig-cache/bin/fltk_app
 ```
+
+## Dependencies
+Check the [cfltk](https://github.com/moalyousef/cfltk) repo for the necessary dependencies on each system/distro.
--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,30 @@
 const c = @cImport({
-  @cInclude("cfl.h");
-  @cInclude("cfl_enums.h");
-  @cInclude("cfl_button.h");
-  @cInclude("cfl_box.h");
-  @cInclude("cfl_window.h");
+    @cInclude("cfl.h"); // for Fl_init_all, Fl_run
+    @cInclude("cfl_enums.h"); // Fl_Color_Red
+    @cInclude("cfl_image.h"); // Fl_register_images
+    @cInclude("cfl_button.h"); // Fl_Button
+    @cInclude("cfl_box.h"); // Fl_Box
+    @cInclude("cfl_window.h"); // Fl_Window
 });
 
 pub fn cb(w: ?*c.Fl_Widget, data: ?*c_void) callconv(.C) void {
-  c.Fl_Box_set_label(@ptrCast(*c.Fl_Box, data), "Hello World!");
-  c.Fl_Button_set_color(@ptrCast(*c.Fl_Button, w), c.Fl_Color_Red);
+    c.Fl_Box_set_label(@ptrCast(*c.Fl_Box, data), "Hello World!");
+    c.Fl_Button_set_color(@ptrCast(*c.Fl_Button, w), c.Fl_Color_Red);
 }
 
-pub fn main() !void {
+pub fn main() void {
+    c.Fl_init_all(); // inits all styles, if needed
+    c.Fl_register_images(); // register image types supported by fltk, if needed
+    _ = c.Fl_lock(); // enable multithreading, if needed
+    
+    c.Fl_set_scheme("gtk+");
     var win = c.Fl_Window_new(100, 100, 400, 300, "Hello");
-    var but = c.Fl_Button_new(160, 200, 80, 40, "Click me!");
-    var box = c.Fl_Box_new(0, 0, 400, 200, "");
+    var but = c.Fl_Button_new(160, 220, 80, 40, "Click me!");
+    var box = c.Fl_Box_new(10, 10, 380, 180, "");
+    c.Fl_Box_set_box(box, c.Fl_BoxType_UpBox);
     c.Fl_Window_end(win);
     c.Fl_Window_show(win);
     c.Fl_Button_set_callback(but, cb, box);
+    
     _ = c.Fl_run();
 }
