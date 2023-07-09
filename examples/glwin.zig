44b7946b23ff9a5c2ce24a8678cb78047f21a358
MoAlyousef <may642_2000@hotmail.com>
2023-07-10 01:04:17 +0300

    add more GlutWindow methods

---
+++ examples/glwin.zig
@@ -0,0 +1,31 @@
+const zfltk = @import("zfltk");
+const app = zfltk.app;
+const GlutWindow = zfltk.GlutWindow;
+const Mode = zfltk.enums.Mode;
+
+extern fn glClearColor(r: f32, g: f32, b: f32, a: f32) void;
+extern fn glClear(v: i32) void;
+const GL_COLOR_BUFFER_BIT = 16384;
+
+fn winDraw(win: *GlutWindow) void {
+    win.makeCurrent();
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+pub fn main() !void {
+    try app.init();
+    var win = try GlutWindow.init(.{
+        .w = 400,
+        .h = 300,
+
+        .label = "Hello OpenGL",
+    });
+    win.setMode(Mode.opengl3 | Mode.multi_sample);
+    win.end();
+    win.resizable(win);
+    win.show();
+
+    win.setDrawHandler(winDraw);
+    try app.run();
+}
--- src/window.zig
+++ src/window.zig
@@ -26,41 +26,21 @@ pub const Window = struct {
     pub inline fn deinit(self: *Window) void {
         c.Fl_Double_Window_delete(self.raw());
     }
+
+    pub fn freePosition(self: *Window) void {
+        c.Fl_Double_Window_free_position(self.raw());
+    }
+
+    pub fn flush(self: *Window) void {
+        c.Fl_Double_Window_flush(self.raw());
+    }
 };
-
-pub fn methods(comptime Self: type) type {
-    return struct {
-        pub fn setSizeRange(self: *Self, min_w: u31, min_h: u31, max_w: u31, max_h: u31) void {
-            return c.Fl_Double_Window_size_range(self.raw(), min_w, min_h, max_w, max_h);
-        }
-
-        pub fn iconize(self: *Self) void {
-            c.Fl_Double_Window_iconize(self.raw());
-        }
-
-        pub fn freePosition(self: *Self) void {
-            c.Fl_Double_Window_free_position(self.raw());
-        }
-
-        pub fn setCursor(self: *Self, cursor: enums.Cursor) void {
-            return c.Fl_Double_Window_set_cursor(self.raw(), @intFromEnum(cursor));
-        }
-
-        pub fn makeModal(self: *Self, val: bool) void {
-            return c.Fl_Double_Window_make_modal(self.raw(), @intFromBool(val));
-        }
-
-        pub fn setFullscreen(self: *Window, val: bool) void {
-            return c.Fl_Double_Window_fullscreen(self.raw(), @intFromBool(val));
-        }
-    };
-}
 
 pub const GlutWindow = struct {
     // Expose methods from `inherited` structs
     pub usingnamespace zfltk.widget.methods(GlutWindow, RawPtr);
     pub usingnamespace zfltk.group.methods(GlutWindow);
-    pub usingnamespace glmethods(GlutWindow);
+    pub usingnamespace methods(GlutWindow);
     pub const RawPtr = *c.Fl_Glut_Window;
 
     pub inline fn init(opts: Widget.Options) !*GlutWindow {
@@ -76,53 +56,99 @@ pub const GlutWindow = struct {
     pub inline fn deinit(self: *GlutWindow) void {
         c.Fl_Glut_Window_delete(self.raw());
     }
+
+    pub fn getProcAddress(self: *GlutWindow, s: [:0]const u8) ?*anyopaque {
+        return c.Fl_Glut_Window_get_proc_address(self.raw(), s);
+    }
+
+    pub fn flush(self: *GlutWindow) void {
+        c.Fl_Glut_Window_flush(self.raw());
+    }
+
+    pub fn setMode(self: *GlutWindow, mode: i32) void {
+        c.Fl_Glut_Window_set_mode(self.raw(), mode);
+    }
+
+    /// Returns the pixels per unit/point
+    pub fn pixelsPerUnit(self: *GlutWindow) f32 {
+        return c.Fl_Glut_Window_pixels_per_unit(self.raw());
+    }
+
+    /// Gets the window's width in pixels
+    pub fn pixelW(self: *GlutWindow) i32 {
+        return c.Fl_Glut_Window_pixel_w(self.raw());
+    }
+
+    /// Gets the window's height in pixels
+    pub fn pixelH(self: *GlutWindow) i32 {
+        return c.Fl_Glut_Window_pixel_h(self.raw());
+    }
+    /// Swaps the back and front buffers
+    pub fn swapBuffers(self: *GlutWindow) void {
+        c.Fl_Glut_Window_swap_buffers(self.raw());
+    }
+
+    /// Sets the projection so 0,0 is in the lower left of the window
+    /// and each pixel is 1 unit wide/tall.
+    pub fn ortho(self: *GlutWindow) void {
+        c.Fl_Glut_Window_ortho(self.raw());
+    }
+
+    pub fn makeCurrent(self: *GlutWindow) void {
+        c.Fl_Glut_Window_make_current(self.raw());
+    }
+
+    /// Returns whether the OpeGL context is still valid
+    pub fn valid(self: *GlutWindow) bool {
+        return c.Fl_Glut_Window_valid(self.raw()) != 0;
+    }
+
+    /// Mark the OpeGL context as still valid
+    pub fn setValid(self: *GlutWindow, v: bool) void {
+        return c.Fl_Glut_Window_set_valid(self.raw(), v);
+    }
+
+    /// Returns whether the context is valid upon creation
+    pub fn contextValid(self: *GlutWindow) bool {
+        return c.Fl_Glut_Window_context_valid(self.raw()) != 0;
+    }
+
+    /// Mark the context as valid upon creation
+    pub fn setContextValid(self: *GlutWindow, v: bool) void {
+        c.Fl_Glut_Window_set_context_valid(self.raw(), v);
+    }
+
+    /// Returns the GlContext
+    pub fn context(self: *GlutWindow) ?*anyopaque {
+        return c.Fl_Glut_Window_context(self.raw());
+    }
+
+    /// Sets the GlContext
+    pub fn setContext(self: *GlutWindow, ctx: ?*anyopaque, destroy_flag: bool) void {
+        c.Fl_Glut_Window_set_context(self.raw(), ctx, destroy_flag);
+    }
 };
 
-pub fn glmethods(comptime Self: type) type {
+pub fn methods(comptime Self: type) type {
     return struct {
         pub fn setSizeRange(self: *Self, min_w: u31, min_h: u31, max_w: u31, max_h: u31) void {
-            return c.Fl_Glut_Window_size_range(self.raw(), min_w, min_h, max_w, max_h);
+            return c.Fl_Window_size_range(self.raw(), min_w, min_h, max_w, max_h);
         }
 
-        pub fn setFullscreen(self: *GlutWindow, val: bool) void {
-            return c.Fl_Glut_Window_fullscreen(self.raw(), @intFromBool(val));
+        pub fn iconize(self: *Self) void {
+            c.Fl_Window_iconize(self.raw());
         }
 
-        pub fn get_proc_address(self: *GlutWindow, s: [:0]const u8) ?*anyopaque {
-            return c.Fl_Glut_Window_get_proc_address(self.raw(), s);
+        pub fn setCursor(self: *Self, cursor: enums.Cursor) void {
+            return c.Fl_Window_set_cursor(self.raw(), @intFromEnum(cursor));
         }
 
-        pub fn flush(self: *GlutWindow) void {
-            c.Fl_Glut_Window_flush(self.raw());
+        pub fn makeModal(self: *Self, val: bool) void {
+            return c.Fl_Window_make_modal(self.raw(), @intFromBool(val));
         }
 
-        pub fn set_mode(self: *GlutWindow, mode: i32) void {
-            c.Fl_Glut_Window_set_mode(self.raw(), mode);
-        }
-
-        /// Returns the pixels per unit/point
-        pub fn pixels_per_unit(self: *GlutWindow) f32 {
-            return c.Fl_Glut_Window_pixels_per_unit(self.raw());
-        }
-
-        /// Gets the window's width in pixels
-        pub fn pixel_w(self: *GlutWindow) i32 {
-            return c.Fl_Glut_Window_pixel_w(self.raw());
-        }
-
-        /// Gets the window's height in pixels
-        pub fn pixel_h(self: *GlutWindow) i32 {
-            return c.Fl_Glut_Window_pixel_h(self.raw());
-        }
-        /// Swaps the back and front buffers
-        pub fn swap_buffers(self: *GlutWindow) void {
-            c.Fl_Glut_Window_swap_buffers(self.raw());
-        }
-
-        /// Sets the projection so 0,0 is in the lower left of the window
-        /// and each pixel is 1 unit wide/tall.
-        pub fn ortho(self: *GlutWindow) void {
-            c.Fl_Glut_Window_ortho(self.raw());
+        pub fn setFullscreen(self: *Window, val: bool) void {
+            return c.Fl_Window_fullscreen(self.raw(), @intFromBool(val));
         }
     };
 }
